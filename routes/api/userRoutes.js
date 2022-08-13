c3ac28e48dd61bad132c13717f1a4a076cc27edb
jfranklin12 <j.franks725@gmail.com>
2022-08-13 14:51:10 -0400

    added to user routes to establish routes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,3 +1,20 @@
 const router = require('express').Router();
+const {
+    getUsers,
+    getSingleUser,
+    createUser,
+    updateUser,
+    deleteUser,
+} = require('../../controllers/userController');
+
+// /api/users
+router.route('/').get(getUsers).post(createUser);
+
+// /api/users/:userId
+router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
+
+//  /api/users/:userId/friends/:friendId
+// add post and delete route
+router.route('/:userId/friends/:friendId')
 
 module.exports = router;
